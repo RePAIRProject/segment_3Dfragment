4132d1875fb3493fd4c5c5c63f07d62c7c569b25
Yaniv Ohayon <yanivohayon8@gmail.com>
2022-11-29 13:11:50 +0200

    stab

---
+++ src/Scripts.cpp
@@ -0,0 +1,6 @@
+#include "Scripts.h"
+
+void segment_intact_surface(std::vector<std::string> all_args)
+{
+
+}
---
+++ src/Scripts.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+#include <vector>
+
+void segment_intact_surface(std::vector<std::string> all_args);
