315a557a670c5be635ce336eb327c6de99e050a4
Yaniv Ohayon <yanivohayon8@gmail.com>
2022-11-29 11:14:04 +0200

    viewing on the fly

--- main.cpp
+++ main.cpp
@@ -80,10 +80,17 @@ int main(int argc, char* argv[])
 	fragment.saveAsObj(fragFolderPath + "\\" + outFileName, intactSurface);
 	std::cout << "Write successfully the output to path " << fragFolderPath << std::endl;
 
-	/*
+	
 	// For debug:
 	igl::opengl::glfw::Viewer viewer;
-	viewer.data().set_mesh(intactSurface.m_Vertices, intactSurface.m_Faces);
+
+	std::map<int, Eigen::RowVector3d> colors;
+	int segID = viewer.append_mesh();
+	viewer.data(segID).set_mesh(intactSurface.m_Vertices,intactSurface.m_Faces);
+	colors.emplace(viewer.data().id, 0.5 * Eigen::RowVector3d::Random().array() + 0.5);
+	viewer.data(segID).set_colors(colors[segID]);
+	viewer.data(segID).show_lines = false;
+
 	viewer.data().set_face_based(true);
-	viewer.launch();*/
+	viewer.launch();
 }
